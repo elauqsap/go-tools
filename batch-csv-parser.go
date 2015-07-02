872c4f0111a15b34c50e8845979860f44e22ca83
elauqsap <pdago@icloud.com>
2015-07-02 00:10:26 -0500

    stripped and went with a regex for parsing instead of looping

--- batch-csv-parser.go
+++ batch-csv-parser.go
@@ -1,101 +1,45 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
 	"os"
+	"regexp"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
 )
 
-var location string
-var done = make(chan bool)      // boolean channel to end the consumer loop
-var matches = make(chan string) // channel for processing regex matches
 const version = "batch-csv-parser v0.1.0 (c) Pasquale D'Agostino"
+const usage = "Given a directory of CSV files and a column to match on" +
+	" a regular expression, this program will print the matches to standard out.\n" +
+	version +
+	"\n\nUsage:\nbatch-csv-parser [options]\n" +
+	"where [options] are:\n" +
+	"\t --col <i>:\t Column to perform the regex match on\n" +
+	"\t --loc <s>:\t Path to the CSV file or directory of files\n" +
+	"\t --reg <s>:\t Regex to match on the provided column (needs to be last argument)\n" +
+	"\t --top <i+>:\t Columns to provide top occurances\n" +
+	"\t --max <i>:\t Number of top values to print (default: 10)"
 
-/* Ideas
-1. 1st channel feeds matches, 2nd channel feeds stats if any
-2. In stats function use a closure to map the values
-3. defer printing of stats until main closes
-*/
+func init() {
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+	log.SetLevel(log.DebugLevel)
+}
+
+// log.WithFields(log.Fields{
+// 	"animal": "walrus",
+// }).Info("A walrus appears")
 
 func main() {
-	/* Add a loop to go through all CSV files
-	   in the directory around the go functions */
-	argParser()
-	go produceMatches(readCSV(location)) //
-	go consumeMatches()                  //
-	<-done                               //
+	parser()
+
 }
 
-func argParser() {
-	if len(os.Args[1:]) == 0 {
-		fmt.Println(os.Args[0] + " --help for details")
-		os.Exit(0)
-	}
-	for _, arg := range os.Args[1:] {
-		switch arg {
-		case "-h", "--help":
-			printHelp()
-		case "-c", "--col":
-		case "-l", "--loc":
-		case "-r", "--reg":
-		case "-t", "--top":
-		case "-m", "--max":
-		case "-v", "--ver":
-			fmt.Println(version)
-			os.Exit(0)
-		default:
-			fmt.Println(arg + " is an invalid argument, see " +
-				os.Args[0] + " --help for details")
-			os.Exit(0)
-		}
-	}
-}
+func parser() {
+	join := strings.Join(append(os.Args), " ")
+	cli := regexp.MustCompile(`(--top ([\d] ?)+|--only ([\d] ?)+|--col \d|--max \d{1,2}|--loc (/.*/|/.*/*.csv)|--reg (.*)$)`)
+	matches := cli.FindAllString(join, -1)
 
-func printHelp() {
-	fmt.Println("Given a directory of CSV files and a column to match on" +
-		" a regular expression, this program will print the matches to standard out.\n" +
-		version +
-		"\n\nUsage:\nbatch-csv-parser [options]\n" +
-		"where [options] are:\n" +
-		"\t --col <i>:\t Column to perform the regex match on\n" +
-		"\t --loc <s>:\t Path to the CSV file or directory of files\n" +
-		"\t --reg <s>:\t Regex to match on the provided column\n" +
-		"\t --top <i+>:\t Columns to provide top occurances\n" +
-		"\t --max <i>:\t Number of top values to print (default: 10)")
-	os.Exit(0)
-}
-
-/*
-
-*/
-func produceMatches(entries [][]string) {
-	for _, each := range entries {
-		matches <- each[0]
-	}
-	done <- true
-}
-
-func consumeMatches() {
-	for {
-		match := <-matches
-		fmt.Println(match)
-	}
-}
-
-func readCSV(file string) [][]string {
-	csvFile, err := os.Open(file)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer csvFile.Close()
-
-	reader := csv.NewReader(csvFile)
-	reader.FieldsPerRecord = -1
-	data, err := reader.ReadAll()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	return data
+	log.WithFields(log.Fields{
+		"Args": matches[1],
+	}).Info("Command Line Input")
 }
